b30f7436d47093df923c17b6208baf1b720aeb74
abir <abirbanti527@gmail.com>
2025-09-11 00:27:32 +0530

    pattern

---
+++ JAVA/New folder/PiramidHollow.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class PiramidHollow {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number --> ");
+        int n = sc.nextInt();
+
+        //#1 this a code--> piramid
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print("  ");
+            }
+            // printing the star
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                // System.out.print("* ");
+
+                if (j==1||i == n || j == 2 * i - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
