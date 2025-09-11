bebcf2c7b9da0fe84d0245e923f6f015925dd1c5
abir <abirbanti527@gmail.com>
2025-09-12 00:31:40 +0530

    khfkhw

--- JAVA/New folder/PiramidHollow.java
+++ JAVA/New folder/PiramidHollow.java
@@ -17,7 +17,7 @@ public class PiramidHollow {
             for (int j = 1; j <= 2 * i - 1; j++) {
                 // System.out.print("* ");
 
-                if (j==1||i == n || j == 2 * i - 1) {
+                if (j == 1 || i == n || j == 2 * i - 1) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
