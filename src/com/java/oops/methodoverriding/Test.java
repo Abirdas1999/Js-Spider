9b391ce343cd0e028a7638eec958fcbdd8f1ca2b
Abirdas1999 <abirbanti527@gmail.com>
2025-09-21 19:35:34 +0530

    2nd

--- src/com/java/oops/methodoverriding/Test.java
+++ src/com/java/oops/methodoverriding/Test.java
@@ -2,6 +2,8 @@ package com.java.oops.methodoverriding;
 
 public class Test {
     static void main(String[] args) {
+
+        System.out.println("This is a Java project");
         System.out.println("This is a Java project");
     }
 }
