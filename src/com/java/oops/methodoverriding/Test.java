e1e334fc58ea7f75fbe021d1a526a77753b8e6eb
Abirdas1999 <abirbanti527@gmail.com>
2025-09-21 19:44:27 +0530

    now ready

--- src/com/java/oops/methodoverriding/Test.java
+++ src/com/java/oops/methodoverriding/Test.java
@@ -5,5 +5,10 @@ public class Test {
 
         System.out.println("This is a Java project");
         System.out.println("This is a Java project");
+        int i = 1;
+        while (i<10){
+            i++;
+            System.out.println(i);
+        }
     }
 }
