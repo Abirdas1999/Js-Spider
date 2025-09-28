5edaab45e2bafd8a416d101bffd7d0d5185fa130
Abirdas1999 <abirbanti527@gmail.com>
2025-09-28 16:24:11 +0530

    28/09/25

---
+++ src/com/javaAbir/oops/interfaceAll/Test.java
@@ -0,0 +1,19 @@
+package com.javaAbir.oops.interfaceAll;
+interface  I1{
+    int i =20;//➡️static final int i
+    void m1();//➡️public abstract void m1()
+}
+class B implements I1{
+    @Override
+    public void m1(){
+        System.out.println("M1()--B");
+    }
+}
+public class Test {
+    public static void main(String[] args){
+        I1 i = new B();
+        i.m1();
+        //I1.i=200;//❌can not re-declare because final can't be changed
+        System.out.println(I1.i);//✅can access because it is a static variable
+    }
+}
