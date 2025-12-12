23b18e346d829b996317a625b480cd2f4a5028d3
Abirdas1999 <abirbanti527@gmail.com>
2025-12-13 00:21:03 +0530

    vhvhj

--- src/com/javaAbir/array/Comparable/Demo.java
+++ src/com/javaAbir/array/Comparable/Demo.java
@@ -1,9 +1,10 @@
 package com.javaAbir.array.Comparable;
 
 import java.util.Arrays;
-class Demo{
+
+class Demo {
     public static void main(String[] args) {
-        Circle [] c = {
+        Circle[] c = {
                 new Circle(30),
                 new Circle(40),
                 new Circle(60),
@@ -12,38 +13,39 @@ class Demo{
                 new Circle(30),
         };
 //        Arrays.sort(c);
-        Sorting s  = new Sorting();
+        Sorting s = new Sorting();//✅creating own sort method of arrays class
         s.sort(c);
-
-
         System.out.println(Arrays.toString(c));
     }
 }
-class Circle implements Comparable <Circle> {
+
+class Circle implements Comparable<Circle> {
     int radius;
-    public Circle(int r){
+
+    public Circle(int r) {
         radius = r;
     }
+
     @Override
-    public int compareTo(Circle c){
+    public int compareTo(Circle c) {
         return radius - c.radius;
     }
 
     @Override
     public String toString() {
-        return "Radius =["+radius+"]";
+        return "Radius =[" + radius + "]";
     }
 }
 
-class Sorting{
-     void  sort (Circle [] a){
-        for (int i = 0; i < a.length-1 ; i++) {
-            for (int j = 0; j < a.length-1-i ; j++) {
-//                Comparable c = (Comparable) a[j];
-                if (a[j].compareTo(a[j+1])<0){
+class Sorting {
+    void sort(Circle[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                //Comparable c = (Comparable) a[j];
+                if (a[j].compareTo(a[j + 1]) < 0) {
                     Circle temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1]=temp;
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
             }
         }
