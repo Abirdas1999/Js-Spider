5fd2ab7178157663973494a50d6638669deea59b
Abirdas1999 <abirbanti527@gmail.com>
2025-12-13 23:47:05 +0530

    Pollymorphism

---
+++ src/com/javaAbir/oops/Pollymorphism/CompileTime/Demo.java
@@ -0,0 +1,29 @@
+package com.javaAbir.oops.Pollymorphism.CompileTime;
+class Student{
+    String name;
+    int id;
+    String course;
+    void getStudent(String name){
+        System.out.println("Student name is --> "+name);
+    }
+    void getStudent(String name,int id){
+        System.out.println("Student name is --> "+name);
+        System.out.println("Student id --> "+id);
+    }
+    void getStudent(String name,int id,String course){
+        System.out.println("Student name is --> "+name);
+        System.out.println("Student id --> "+id);
+        System.out.println("Student Course --> "+course);
+    }
+
+}
+class Demo {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.getStudent("Abir");
+        s.getStudent("Abir",23);
+        s.getStudent("Abir",34,"Java");
+
+    }
+
+}
